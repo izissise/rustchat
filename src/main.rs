85d9da14a52d9a354bb9f53de87b7fe8970c8b7e
Hugues Morisset <morisset.hugues@gmail.com>
2018-05-12 21:05:44 +0200

    BAsic listening socket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate tokio;
+
+use tokio::io;
+use tokio::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let listen_addr = "0.0.0.0:2435".parse().unwrap();
+    let listener = tokio::net::TcpListener::bind(&listen_addr).unwrap();
+
+    let server = listener.incoming().for_each(|socket| {
+        println!("New connection: {:?}", socket.peer_addr().unwrap());
+        let message = "Ok\n";
+        let peer_write = io::write_all(socket, message)
+            .then(|res| {
+//                 println!("Wrote {:?} bytes", message.len());
+                println!("Wrote {:?}", res.is_ok());
+                Ok(())
+            });
+        tokio::spawn(peer_write);
+        Ok(())
+    }).map_err(|err| {
+        println!("accept() error => {:?}", err);
+    });
+    tokio::run(server);
 }
